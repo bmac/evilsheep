8615f0fc60a2a558137b0cc1a97e8c6a45076400
Brendan McLoughlin <bmac325@gmail.com>
2010-08-29 00:57:57 -0400

    create cartitem db table

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from catalog import Product
 # Create your models here.
+
+
+class CartItem(models.Model):
+    cart_id  = models.CharField(max_length=100)
+    date_added = models.DateField(auto_now_add=True)
+    quantity = models.IntegerField(default=1)
+    product = models.ForeignKey('catalog.product', unique=False)
+    
+    class Meta:
+        ordering = ['date_added']
+        
+
+    def total(self):
+        return self.quantity * self.product 
+    
+    def name(self):
+        return self.product.name
+    
+    def price(self):
+        return self.product.price
+
+    def get_absolute_url(self):
+        return self.product.get_absolute_url()
+    
+    def augement_quantity(self, quantity):
+        self.quantity = self.quantity + int(quantity)
+        self.save()
+
+    
+        
