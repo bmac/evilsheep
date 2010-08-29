fb4dd69b288bf67807480d624fc857d67aa4dca9
Brendan McLoughlin <bmac325@gmail.com>
2010-08-29 00:15:05 -0400

    Added category db models

--- catalog/models.py
+++ catalog/models.py
@@ -8,9 +8,13 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=9, decimal_places=2)
     description = models.TextField()
     brand = models.CharField(blank=True, max_length=255)
+    category = models.ForeignKey('category.SubCategory')
+
 
     is_active = models.BooleanField(default=True)
     is_featured = models.BooleanField(default=False)
+    bonus_offer = models.ForeignKey('category.SuperCategory', blank=True, null=True)
+
     image = models.ImageField()
     thumbnails = models.ImageField()
     
@@ -41,3 +45,54 @@ class Product(models.Model):
         return ('products_product', (), {'product_slug': self.slug })
 
 
+class SuperCategory(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+    slug = models.SlugField()
+    is_active = models.BooleanField(default=True)
+    
+    class Meta:
+        ordering = ['name']
+        
+
+    def __unicode__(self):
+        return self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('catelog_supercategory', (), {'supercat_slug': self.slug })
+
+
+class SubCategory(model.Model):
+    name = models.CharField(unique=True, max_length=100)
+    slug = models.SlugField()
+    is_active = models.BooleanField(default=True)
+    supercat = models.ForeignKey(SuperCategory)
+    
+    product_queue = models.ForeignKey(Product)
+
+    class Meta:
+        ordering = ['name']
+        
+
+    def __unicode__(self):
+        return self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('catelog_subcategory', (), {'subcat_slug': self.slug })
+
+
+
+class Book(Product):
+    author = models.CharField(max_length=255)
+    binding = models.CharField(blank=True, max_length=255)
+    edition = models.CharField(blank=True, max_length=255)
+    language = models.CharField(blank=True, max_length=100)
+    pages = models.IntegerField(blank=True, null=True)
+    publisher = models.CharField(blank=True, max_length=255)
+    subject = models.CharField(blank=True, max_length=255)
+    
+    
+        
+        
+        
