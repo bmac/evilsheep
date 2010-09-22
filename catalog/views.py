5fc0713ccb2bb1f34471cfb9dd836432a400e7d8
Brendan McLoughlin <bmac325@gmail.com>
2010-09-22 17:37:08 -0400

    added pagination to sub_cat_view

--- catalog/views.py
+++ catalog/views.py
@@ -6,6 +6,8 @@ from django.http import HttpResponseRedirect
 from django.core import urlresolvers
 from evilsheep.cart import cart
 from catalog.forms import AddProductToCartForm
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+
 
 def show_product(request, supercat_slug, subcat_slug, product_slug):
     product = get_object_or_404(Product, slug=product_slug, category__slug=subcat_slug, category__super_category__slug=supercat_slug, is_active=True)
@@ -43,10 +45,29 @@ def show_super_cat(request, supercat_slug):
 
 
 def show_sub_cat(request, supercat_slug, subcat_slug):
-    current_cat = get_object_or_404(SubCategory, slug=subcat_slug, super_category__slug=supercat_slugm, is_active=True)
+
+    current_cat = get_object_or_404(SubCategory, slug=subcat_slug, 
+                   super_category__slug=supercat_slugm, is_active=True)
+    products = Product.active.filter(category__slug=subcat_slug).order_by('name')
+    paginator = Paginator(products, 16) # Show 8 contacts per page
+
+    # Make sure page request is an int. If not, deliver first page.
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    # If page request (9999) is out of range, deliver last page of results.
+    try:
+        products = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        products = paginator.page(paginator.num_pages)
+
+    current_cat = get_object_or_404(SubCategory, slug=subcat_slug, super_category__slug=supercat_slug, is_active=True)
     sub_categories = SubCategory.active.filter(super_category__slug=supercat_slug)
     meta_keywords = current_cat.meta_keywords
     meta_description = current_cat.meta_description
+    
     return render_to_response('catalog/sub_cat.html', locals(), context_instance=RequestContext(request))
 
 
