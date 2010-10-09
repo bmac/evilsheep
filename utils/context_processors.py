cc81c2db7792398156aabf26fd2f52c49c0c9a11
Brendan McLoughlin <bmac325@gmail.com>
2010-10-09 13:35:27 -0400

    added cart info to the context preprocessor

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,8 +1,13 @@
 from evilsheep import settings
+from evilsheep.cart import cart
+
 
 def ecomstore(request):
+    item_count = cart.cart_distinct_item_count(request)
+    
     return {
         'site_name' : settings.SITE_NAME,
         'meta_keywords' : settings.META_KEYWORDS,
         'meta_description' : settings.META_DESCRIPTION,
+        'item_count' : item_count,
         }
