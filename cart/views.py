92880f5d0863f173dd027fe338b543fead3584d7
Brendan McLoughlin <bmac325@gmail.com>
2010-10-10 01:48:16 -0400

    cart now correctly calculates the subtotal

--- cart/views.py
+++ cart/views.py
@@ -8,5 +8,5 @@ def show_cart(request):
     page_title = 'Shopping Cart'
     cart_item_count = cart.cart_distinct_item_count(request)
     cart_items = cart.get_cart_items(request)
-    cart_subtotal = 0
+    cart_subtotal = cart.cart_subtotal(request)
     return render_to_response('cart/cart.html', locals(), context_instance=RequestContext(request))
