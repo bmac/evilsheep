d5d9b0870b58c781ef174dce665214d24f03ab80
Brendan McLoughlin <bmac325@gmail.com>
2010-10-16 15:55:28 -0400

    added flatpages to settings

--- settings.py
+++ settings.py
@@ -81,6 +81,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 )
 
 ROOT_URLCONF = 'evilsheep.urls'
@@ -100,7 +101,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.admin',
-
+    'django.contrib.flatpages',
+    
     #Custom apps
     'evilsheep.catalog',
     'evilsheep.utils',
